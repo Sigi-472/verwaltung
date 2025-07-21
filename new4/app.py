80e90ac2c89faf99ab8de458042ef2511a20e5c3
n <norman.koch@tu-dresden.de>
2025-07-21 20:47:26 +0200

    fix

---
+++ new4/app.py
@@ -0,0 +1,26 @@
+from db_defs import *
+from db_helpers import describe_possible_joins, add_all_and_commit, add_and_commit, delete_and_commit, execute_and_commit, generate_editable_table
+
+# ========== MAIN ==========
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///mydatabase.db", echo=False)
+    Base.metadata.create_all(engine)
+
+    describe_possible_joins("person", Base)
+
+    with Session(engine) as session:
+        query = session.query(Person).outerjoin(Person.contacts)  # Beispiel: `contacts` ist Beziehung zu PersonContact
+        rows = query.all()
+
+        columns = [
+            ("Person", "first_name", "First Name", False),
+            ("Person", "last_name", "Last Name", False),
+            ("Person", "comment", "Comment", False),
+            ("contacts", "email", "Emails", True),
+            ("contacts", "fax", "Faxes", True),
+        ]
+
+
+        html_table = generate_editable_table(rows, columns, id_column='id')
+
+        print(html_table)
