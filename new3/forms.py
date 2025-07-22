1de69e9384c71fb214e9c68c71a0c86a57324a60
n <norman.koch@tu-dresden.de>
2025-07-22 06:56:05 +0200

    fix

--- new3/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
-from wtforms.validators import DataRequired
-
-class AssignmentForm(FlaskForm):
-    person_id = SelectField("Person", coerce=int, validators=[DataRequired()])
-    abteilung_id = SelectField("Abteilung", coerce=int, validators=[DataRequired()])
-    submit = SubmitField("Speichern")
